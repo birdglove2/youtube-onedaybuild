d03e9dd722caf964a5a4281ebd6cd7f6646d4607
Krittamook Aksornchindarat <krittamook@hotmail.com>
2020-07-28 14:52:50 +0700

    initial xCode project creation

---
+++ youtube-onedaybuild/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-onedaybuild
+//
+//  Created by Krittamook Aksornchindarat on 28/7/2563 BE.
+//  Copyright © 2563 Krittamook Aksornchindarat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
